6026448e5d6db5ec50ae0ddb68af164fb8ebaf88
Jayprakash Sahu <118419073+Jpfpp12@users.noreply.github.com>
2025-05-21 14:09:23 +0000

    Fix Redux store import and Provider setup

--- webpack.config.js
+++ webpack.config.js
@@ -31,8 +31,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
+            template: './src/index.html', 
+        }),
     ],
     resolve: {
         extensions: ['.js', '.jsx'],
