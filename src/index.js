892e252281ef5b67bfb84d80f5a82015f5be98a6
Jayprakash Sahu <118419073+Jpfpp12@users.noreply.github.com>
2025-05-21 13:41:55 +0000

    Fix Redux store import and Provider setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { store } from './components/store.js'; 
+import App from './components/App.js';
 
 ReactDOM.render(
   <Provider store={store}>
